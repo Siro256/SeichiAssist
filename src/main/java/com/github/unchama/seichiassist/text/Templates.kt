fc492fe73c3e19f9b5c02f6e93e945e32be081c9
karayuumac <y-kara0706@docomo.ne.jp>
2019-06-09 09:14:08 +0900

    [CHANGE] TemplatesをKotlin化

---
+++ src/main/java/com/github/unchama/seichiassist/text/Templates.kt
@@ -0,0 +1,18 @@
+package com.github.unchama.seichiassist.text
+
+import java.util.Arrays
+
+import org.bukkit.ChatColor.DARK_GRAY
+import org.bukkit.ChatColor.GREEN
+
+/**
+ * Created by karayuu on 2019/05/03
+ */
+object Templates {
+    val playerInfoDescrpition = Arrays.asList(
+        "$DARK_GRAY※1分毎に更新",
+        "${GREEN}統計データは",
+        "${GREEN}各サバイバルサーバー間で",
+        "${GREEN}共有されます"
+    )
+}
